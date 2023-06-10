c2ff911eddd77f926d0c291d07ef99a578f4d8ad
sssemion <ssemi.on@yandex.ru>
2023-06-11 01:55:39 +0300

    sudoku generation service

---
+++ generation_service/tests/test_sudoku.py
@@ -0,0 +1,54 @@
+from generation_service.sudoku import SudokuField, Difficulty
+
+
+def test_generation():
+    field = SudokuField(3, Difficulty.EASY)
+    field.generate_field()
+
+    for row in field:
+        assert sorted(row) == list(range(1, 10))
+
+    cols = [[] for _ in range(9)]
+    for i in range(len(field)):
+        for j in range(len(field[i])):
+            cols[j].append(field[i][j])
+
+    for col in cols:
+        assert sorted(col) == list(range(1, 10))
+
+    for sector_i in range(3):
+        for sector_j in range(3):
+            sector = set()
+            for i in range(sector_i * 3, sector_i * 3 + 3):
+                for j in range(sector_j * 3, sector_j * 3 + 3):
+                    sector.add(field[i][j])
+            assert len(sector) == 9
+
+    field._transpose()
+    assert cols == field.field
+
+
+def test_strike_out():
+    easy_field = SudokuField(3, Difficulty.EASY)
+    easy_field.generate_field()
+    easy_field.strike_out()
+    empty_cell_count = 0
+    for row in easy_field:
+        empty_cell_count += row.count(0)
+    assert 30 <= empty_cell_count < 35
+
+    medium_field = SudokuField(3, Difficulty.MEDIUM)
+    medium_field.generate_field()
+    medium_field.strike_out()
+    empty_cell_count = 0
+    for row in medium_field:
+        empty_cell_count += row.count(0)
+    assert 25 <= empty_cell_count < 30
+
+    hard_field = SudokuField(3, Difficulty.HARD)
+    hard_field.generate_field()
+    hard_field.strike_out()
+    empty_cell_count = 0
+    for row in hard_field:
+        empty_cell_count += row.count(0)
+    assert 20 <= empty_cell_count < 25
