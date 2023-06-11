20b516da89e810891e618a09c4937bc61e470a11
sssemion <ssemi.on@yandex.ru>
2023-06-12 00:16:26 +0300

    sidable sber

--- sudoku/src/App.js
+++ sudoku/src/App.js
@@ -1,57 +1,54 @@
 import React,{ useState, Suspense } from 'react';
 
-import {
-    createSmartappDebugger,
-    createAssistant,
-} from "@salutejs/client";
+// import {
+//     createSmartappDebugger,
+//     createAssistant,
+// } from "@salutejs/client";
 
 import "./App.css";
 
-import SudokuBoard from './components/SudokuBoard';
-import MyButton from './components/MyButton';
-import Strings from './constants/strings';
 import { Layout } from './components/Layout';
+//
+// const initializeAssistant = (getState/*: any*/) => {
+//     if (process.env.NODE_ENV === "development") {
+//         return createSmartappDebugger({
+//         token: process.env.REACT_APP_TOKEN,
+//         initPhrase: `Запусти ${process.env.REACT_APP_SMARTAPP}`,
+//         getState,
+//         });
+//     }
+//     return createAssistant({ getState });
+// };
 
-const initializeAssistant = (getState/*: any*/) => {
-    if (process.env.NODE_ENV === "development") {
-        return createSmartappDebugger({
-        token: process.env.REACT_APP_TOKEN,
-        initPhrase: `Запусти ${process.env.REACT_APP_SMARTAPP}`,
-        getState,
-        });
-    }
-    return createAssistant({ getState });
-};
-  
 
 export default class App extends React.Component {
 
     constructor(props) {
         super(props);
         this.board = Array(9).fill(Array(9).fill(12));
-          
+
         this.state = {}
 
-        this.assistant = initializeAssistant(() => this.getStateForAssistant() );
+        // this.assistant = initializeAssistant(() => this.getStateForAssistant() );
     }
-    
+
 
     // handleCellChange = (row, col, value) => {
     //     const newBoard = this.board.map((r, i) => (i === row ? [...r.slice(0, col), value, ...r.slice(col + 1)] : r));
     //     this.setBoard(newBoard);
     // };
 
-    
+
     render() {
           return (
               <Layout/>
           );
-        
+
     }
 
-    getStateForAssistant () {
-        const state = {};
-        return state;
-    }
+    // getStateForAssistant () {
+    //     const state = {};
+    //     return state;
+    // }
 }
 
